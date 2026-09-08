7141529153d7a8f4267596227185851e0c4c7f47
Luis david florez <luisitoflorez12@gmail.com>
2026-09-08 17:24:34 -0500

    quitar barra

---
+++ static/sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'comfesocial-v1';
+const APP_SHELL = ['/static/style_new.css', '/static/manifest.json', '/static/icon-192.svg', '/static/icon-512.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
